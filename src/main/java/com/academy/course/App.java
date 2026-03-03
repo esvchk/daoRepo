f36da0ed231c8f0d2856a39ba72734522ab5d4a1
User <esvchk@gmail.com>
2026-03-03 20:12:55 +0300

    dao

--- src/main/java/com/academy/course/App.java
+++ src/main/java/com/academy/course/App.java
@@ -1,9 +1,5 @@
 package com.academy.course;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/academy/course/dao/CarDao.java
@@ -0,0 +1,7 @@
+package com.academy.course.dao;
+
+import com.academy.course.dto.Car;
+
+public interface CarDao extends DAO<Car>{
+
+}
