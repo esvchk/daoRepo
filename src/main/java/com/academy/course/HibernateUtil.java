f80f472bd0392c449e0314b1a572dfc615f92169
User <esvchk@gmail.com>
2026-03-10 21:48:12 +0300

    HibernateUtil

---
+++ src/main/java/com/academy/course/HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.academy.course;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    public static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
+            Persistence.createEntityManagerFactory("academy");
+
+    public static EntityManager getEntityManager() {
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+
+    public static void close() {
+        ENTITY_MANAGER_FACTORY.close();
+    }
+}
