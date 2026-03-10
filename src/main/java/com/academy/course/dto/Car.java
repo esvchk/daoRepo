ff5804f421664d807240e0409f9292c327efbe55
User <esvchk@gmail.com>
2026-03-10 21:47:30 +0300

    adding annotations in car class

--- src/main/java/com/academy/course/dto/Car.java
+++ src/main/java/com/academy/course/dto/Car.java
@@ -5,13 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Car {
-    private long id;
+@Entity
+@Table
+public class Car implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "type")
     private String type;
 
 }
