2ea99adf92464dcf73e6338854b0a35da827f779
User <esvchk@gmail.com>
2026-03-19 20:16:14 +0300

    hierarchy and inheritance

--- src/main/java/com/academy/course/AppHibernate.java
+++ src/main/java/com/academy/course/AppHibernate.java
@@ -2,6 +2,10 @@
 package com.academy.course;
 
 import com.academy.course.dto.Car;
+import com.academy.course.hierarchy.Address;
+import com.academy.course.hierarchy.Employee1;
+import com.academy.course.hierarchy.Person1;
+import com.academy.course.hierarchy.Student;
 import org.hibernate.Session;
 
 
@@ -9,34 +13,55 @@ import javax.persistence.EntityManager;
 
 public class AppHibernate {
     public static void main(String[] args) {
-        Car car = Car.builder()
-                .name("Corolla")
-                .type("Toyota")
-                .build();
+//        Car car = Car.builder()
+//                .name("Corolla")
+//                .type("Toyota")
+//                .build();
         EntityManager em = HibernateUtil.getEntityManager();
+//        em.getTransaction().begin();
+//        em.persist(car);
+//        car.setType("Toyota new");
+//        em.merge(car);
+//        em.getTransaction().commit();
+
+        Student student = Student.builder()
+                .mark(9.5)
+                .faculty("Literature")
+                .build();
+
+        Employee1 employee1 = Employee1.builder()
+                .salary(200.10)
+                .company("corporation")
+                .build();
+
+        Person1 person1 = Person1.builder()
+                .name("Sam")
+                .surname("samuel")
+                .build();
+//
+//
+//
+//        Car carNew = Car.builder()
+//                .name("Corolla2")
+//                .type("Toyota2")
+//                .build();
+//        Session session = em.unwrap(Session.class);
+//        session.getTransaction().begin();
+//        session.save(carNew);
+//        Car carDb = session.get(Car.class, carNew.getId());
+//        Car carDbLoad = session.load(Car.class,carNew.getId());
+//        System.out.println(carDb);
+//        carDb.setType("new");
+//        session.refresh(carDb);
+//        session.saveOrUpdate(carNew);
+//        session.delete(carNew);
+//        session.getTransaction().commit();
         em.getTransaction().begin();
-        em.persist(car);
-        car.setType("Toyota new");
-        em.merge(car);
+        em.persist(student);
+        em.persist(employee1);
+        em.persist(person1);
         em.getTransaction().commit();
 
-
-
-        Car carNew = Car.builder()
-                .name("Corolla2")
-                .type("Toyota2")
-                .build();
-        Session session = em.unwrap(Session.class);
-        session.getTransaction().begin();
-        session.save(carNew);
-        Car carDb = session.get(Car.class, carNew.getId());
-        Car carDbLoad = session.load(Car.class,carNew.getId());
-        System.out.println(carDb);
-        carDb.setType("new");
-        session.refresh(carDb);
-        session.saveOrUpdate(carNew);
-        session.delete(carNew);
-        session.getTransaction().commit();
 
 
         em.close();
--- src/main/java/com/academy/course/hierarchy/Person1.java
+++ src/main/java/com/academy/course/hierarchy/Person1.java
@@ -1,20 +1,27 @@
 package com.academy.course.hierarchy;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.io.Serializable;
-
+@SuperBuilder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+//@DiscriminatorColumn(name = "PERSON_TYPE",discriminatorType = DiscriminatorType.CHAR)
+//@DiscriminatorValue("P")
 @Entity
 @Table(name = "Person1")
 public class Person1 implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column
